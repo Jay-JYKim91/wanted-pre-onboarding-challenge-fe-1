09e89081a6cc8012f698ba56b9b23d50d93c0428
Jay-JYKim91 <jay.jykim91@gmail.com>
2022-08-10 21:09:56 +0900

    DOCS: update readme.md

---
+++ .history/src/services/authAPI_20220810210657.ts
@@ -0,0 +1,21 @@
+import { AuthData } from '../page/Auth';
+
+export async function logIn(data: AuthData) {
+  const response =  await fetch("http://localhost:8080/users/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).catch(error => alert(error.message));
+  const result = await response.json();
+  return result;
+}
+
+export async function signUp(data: AuthData) {
+  const response = await fetch("http://localhost:8080/users/create", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  }).catch(error => alert(error.message));
+  const result = await response.json();
+  return result;
+}
